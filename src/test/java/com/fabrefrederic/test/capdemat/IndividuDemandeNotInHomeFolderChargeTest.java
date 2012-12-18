8bea1c9528034b683add7e9756dd4a721aaecf0c
Fred <frederic@frederic-OptiPlex-380>
2012-12-18 11:15:51 +0100

    Test monté en charge sur les demandes

---
+++ src/test/java/com/fabrefrederic/test/capdemat/IndividuDemandeNotInHomeFolderChargeTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.fabrefrederic.test.capdemat;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+/**
+ * @author Inexine : Frederic Fabre
+ * 
+ */
+public class IndividuDemandeNotInHomeFolderChargeTest extends AbstractInexineTest {
+    private static Logger logger = Logger.getLogger(IndividuDemandeNotInHomeFolderChargeTest.class);
+
+    @Test
+    public void connexionDeconnexion() {
+	Integer compteur = 0;
+	
+	try {
+	    AbstractInexineTest.initialisation();
+	}
+	catch (Exception exceptionInitilisation) {
+	    logger.error(exceptionInitilisation.getMessage());
+	}
+	
+	for (int i = 0; i < 500; i++) {
+	    try {
+		AbstractInexineTest.login();
+		AbstractInexineTest.driver.get(baseUrl + "/frontoffice/home");
+		AbstractInexineTest.driver.findElement(By.linkText("Déconnexion")).click();
+	    }
+	    catch (Exception e) {
+		logger.error(e.getMessage());
+	    }
+	    compteur = compteur + 1;
+	    System.out.println(compteur);
+	}
+    }
+
+}
